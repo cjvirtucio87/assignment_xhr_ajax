141eaba6d5c429f8822bcde7a5bf7cc6aac272ac
cjvirtucio87 <cjv287@gmail.com>
2016-09-16 10:25:07 -0700

    Working get helper.

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ var dollarSign = {
   ajax: function(options) {
     options = options || {};
 
+    // defaults
+    options.complete = options.complete || function(xhr) {
+      console.log(xhr.responseText);
+    };
+
+    options.success = options.success || function() {
+      console.log('this is successful');
+    };
+
+    options.error = options.error || function() {
+      console.log('this is an error');
+    };
+
     var xhr = new XMLHttpRequest();
 
     xhr.onload = function ( e ) {
@@ -32,27 +45,49 @@ var dollarSign = {
       xhr.setRequestHeader(key, options.headers[key]);
     }
 
+    if (options.dataType) {
+      xhr.responseType = dataType;
+    }
+
     options.data = options.data || '';
 
     xhr.send(options.data);
 
   },
+
+  get: function(url, data, callback, dataType) {
+    dollarSign.ajax({url: url,
+                    data: data,
+                    method: 'GET',
+                    success: callback,
+                    dataType: dataType });
+  }
 };
 
-dollarSign.ajax({
-  method: 'POST',
-  url: "http://reqres.in/api/users",
-  complete: function(xhr) {
-    console.log(xhr.responseText);
-  },
-  success: function() {
-    console.log('this is successful');
-  },
-  error: function() {
-    console.log('this is an error');
-  },
-  headers: {
-  },
-  data: '',
-  async: true
-});
+
+// regular ajax
+// dollarSign.ajax({
+//   method: 'POST',
+//   url: "http://reqres.in/api/users",
+//   complete: function(xhr) {
+//     console.log(xhr.responseText);
+//   },
+//   success: function() {
+//     console.log('this is successful');
+//   },
+//   error: function() {
+//     console.log('this is an error');
+//   },
+//   headers: {
+//   },
+//   data: '',
+//   async: true
+// });
+
+// get helper
+dollarSign.get(
+  "http://reqres.in/api/users",
+  '',
+  function() {
+    console.log('from the get request');
+  });
