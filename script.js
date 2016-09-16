8eb033fe59c8e8ea2d97a73bf4d6391a08b39b2e
cjvirtucio87 <cjv287@gmail.com>
2016-09-16 10:10:57 -0700

    Tested post request.

--- script.js
+++ script.js
@@ -6,29 +6,41 @@ var dollarSign = {
 
     xhr.onload = function ( e ) {
       if ( xhr.readyState === 4 ) {
-        if ( xhr.status === 200 ) {
-          options.success();
+        if ( xhr.status === 200 || xhr.status === 201 ) {
+          if (options.success) {
+            options.success(xhr);
+          }
         } else {
-          options.error();
+          console.log(xhr.status);
+          if (options.error) {
+            options.error(xhr);
+          }
         }
-        options.complete(xhr);
+        if (options.complete) {
+          options.complete(xhr);
+        }
       }
     };
 
+    options.async = options.async || true;
+
     xhr.open(options.method, options.url, options.async);
+
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
     for (var key in options.headers) {
       xhr.setRequestHeader(key, options.headers[key]);
     }
+
+    options.data = options.data || '';
 
     xhr.send(options.data);
 
   },
 };
 
-
 dollarSign.ajax({
-  method: 'GET',
+  method: 'POST',
   url: "http://reqres.in/api/users",
   complete: function(xhr) {
     console.log(xhr.responseText);
